4bd7f7ce4a263925ea463307dfb7d370fab12025
Ashfa M Nadaf <nadafashfa@gmail.com>
2026-07-16 20:48:43 +0530

    Initialize UART and SPI, read ADC values
    Added UART and SPI initialization to main function. Implemented ADC reading and conversion to percentage for UART transmission.

--- main.c
+++ main.c
@@ -1,7 +1,31 @@
 #include <LPC21xx.H>
 #include "types.h"
-#include "lcd.h"
-#include "uart.h"
 #include "adc.h"
+#include "uart.h"
 #include "spi.h"
 #include "delay.h"
+
+int main()
+{
+    u16 temp;
+    float a,temp1;
+
+    adc_init();
+    uart0_init(9600);
+    spi0_init();
+
+    while(1)
+    {
+        temp = mcp3204_adc_read(2);
+
+        temp1 = temp;
+
+        a = (((4095 - temp1) / 4095) * 100);
+
+        uart0_tx_string("\r\nL:");
+        uart0_float(a);
+        uart0_tx('%');
+
+        delay_ms(100);
+    }
+}
