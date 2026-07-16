db327a4cb07516cae7ce99a0013ab84a89eb58ce
Ashfa M Nadaf <nadafashfa@gmail.com>
2026-07-16 20:36:30 +0530

    Add types.h with custom type definitions

---
+++ types.h
@@ -0,0 +1,10 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+typedef unsigned int u32;
+typedef signed int s32;
+typedef unsigned char u8;
+typedef signed char s8;
+typedef unsigned short int u16;
+
+#endif
